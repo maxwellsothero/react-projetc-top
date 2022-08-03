83dc7c150e84e2d7006ffb28435cf5b2cf2d4579
unk-overrider <lbrigido05@gmail.com>
2022-08-02 21:53:10 -0300

    conclui a pagina de cadastro com o link de voltar para o inicio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Link to="/">Inicio</Link>
+      <Link to="/cadastro">Cadastro</Link>
 
       <Routes>
         <Route path="/" element={ <Home/> }   />
+        <Route path="/cadastro" element={ <Register/> }   />
       </Routes>
     </BrowserRouter>
   );
