92affe54aa127f1e74398cb4166f0945572c3ebb
Pedro Caio <rachel.alb.97@hotmail.com>
2022-08-02 21:53:58 -0300

    Criando tela de login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-<<<<<<< HEAD
+import Help from "./pages/Help";
+
 import './style.css';
 
 
@@ -11,26 +12,16 @@ import './style.css';
 export default function App() {
   return (
     <div className="app">
-=======
-import Help from "./pages/Help";
+        <Link to="/">Inicio</Link>
+        <Link to="/help">Ajuda</Link>
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Link to="/">Inicio</Link>
-      <Link to="/help">Ajuda</Link>
->>>>>>> 190d234e5d3bd5d07c9fdaf5f84a8b92ff993c5b
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={ <Home/> }   />
-<<<<<<< HEAD
-        <Route path="/login" element={ <Login/> }   />
-=======
-        <Route path="/help" element={<Help/>} />
->>>>>>> 190d234e5d3bd5d07c9fdaf5f84a8b92ff993c5b
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Home/> }   />
+          <Route path="/login" element={ <Login/> }   />
+          <Route path="/help" element={<Help/>} />
+        </Routes>
+      </BrowserRouter>
     </div>  
   );
 }
