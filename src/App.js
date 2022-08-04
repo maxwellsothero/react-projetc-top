38ee5433ad33949a99ebebf633c5cf5f9b131c9d
osmar mendes <localizameucarro@gmail.com>
2022-08-04 19:32:47 -0300

    menu01

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
+import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 import Contact from "./pages/Contact";
 import Help from "./pages/Help";
 
+import './style.css';
+import Navbar from "./components/Navbar";
+
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Link to="/">Inicio</Link>
-      <Link to="/help">Ajuda</Link>
-      <Routes>
-        <Route path="/" element={ <Home/> }   />
-        <Route path="/contato" element={ <Contact/> }   />
-        <Route path="/help" element={<Help/>} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <div className="app">
+      <Navbar/>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Home/> }   />
+          <Route path="/login" element={ <Login/> }   />
+          <Route path="/contato" element={ <Contact/> }   />
+          <Route path="/help" element={<Help/>} />
+          <Route path="/cadastro" element={ <Register/> }   />
+
+        </Routes>
+      </BrowserRouter>
+    </div>  
+  )
 }
 
-// export default App;
+
---
+++ src/pages/Register/index.js
@@ -0,0 +1,53 @@
+import { Button, Card, Grid, TextField, Typography, Link } from "@mui/material";
+import { Container } from "@mui/system";
+
+
+import "./styles.css";
+
+
+export default function Register () {
+    return (
+        <Container fixed>
+        <Card className="card" variant="outlined" >{
+            
+        <div className="input_cadastro" align="center" >
+            <Typography className="text" variant="h4" >Digite Seus Dados</Typography>
+        
+        <TextField className="input_nome" fullWidth id="outline-basic" label="Nome" variant="outlined"/>
+        <TextField className="input_email"  fullWidth id="outline-basic" label="Email" variant="outlined"/>
+        <TextField className="input_telefone" fullWidth id="outline-basic" label="Telefone" variant="outlined"/>
+        <TextField className="input_senha" fullWidth id="outline-basic" label="Senha" variant="outlined"/>
+            
+        
+        <Grid container>
+        <Grid item xs={6}><Link href="#">VOLTAR AO INICIO</Link></Grid>
+        <Grid item xs={6}><Button variant="outlined">Cadastrar</Button></Grid>
+            
+
+        </Grid>
+        
+        
+        
+        
+     
+        
+    </div>} </Card>
+    </Container>
+
+    )
+    
+    }
+
+
+    /* < div className="input_cadastro" align="center" >
+                <Card variant="outlined">
+                <TextField className="input_nome" fullWidth id="outline-basic" label="Nome" variant="outlined"/>
+                <TextField className="input_email"  fullWidth id="outline-basic" label="Email" variant="outlined"/>
+                <TextField className="input_telefone" fullWidth id="outline-basic" label="Telefone" variant="outlined"/>
+                <TextField className="input_senha" fullWidth id="outline-basic" label="Senha" variant="outlined"/>
+                </Card>
+             
+                
+            </div>*/
+    
+
