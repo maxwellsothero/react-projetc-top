20dfef0de5ad271057dcbee345fdd68501b34553
Pedro Caio <rachel.alb.97@hotmail.com>
2022-08-04 20:29:04 -0300

    criando grafico

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import {Favorite, Home, Sell} from "@mui/icons-material";
+import {Home, Sell} from "@mui/icons-material";
 import { Drawer, Divider, List, ListItemButton, ListItemText, ListItemIcon } from '@mui/material';
 
 import "./styles.css";
